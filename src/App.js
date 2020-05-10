35ad3bef6040bfbdc7bac9f1b7d76abfa39a65ce
Nathan Ward <nathan.ward@puppet.com>
2020-05-10 13:26:05 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as Tone from 'tone';
+import { getMidiNote, getScale, getChord } from 'midi-tools';
 import './App.css';
 
 function App() {
+  const synth = new Tone.PolySynth().toMaster();
+  const chord = getChord('a', 4, 'melodic');
+  synth.triggerAttackRelease(
+    chord.map((noteNumber) => Tone.Frequency(noteNumber, 'midi')),
+    '4n',
+  );
+
   return (
-    <div className="App">
+    <div className='App'>
+      <div>{getMidiNote('a', 4)}</div>
+      <div>{JSON.stringify(getScale('a', 4, 'minor'))}</div>
+      <div>
+        <button onClick={() => Tone.context.resume()}>Play</button>
+      </div>
     </div>
   );
 }
